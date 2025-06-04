d905c5ab952d801467e37bcce6fb9919a0645361
Henry <102833022+SeizeOP@users.noreply.github.com>
2025-06-04 04:44:18 -0400

    Various Updates
    More work on fixing up the automation of the bluelight and greyscale scripts. Plus added a clickable shortcut to rofi in my waybar

--- hypr/scripts/schedule-shaders.sh
+++ hypr/scripts/schedule-shaders.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Fetch the current hour
 current_hour=$(date +"%H0000" | cut -c1-4)
 
-# If it is 1700 to 0900 disable greyscale-shader.glsl, otherwise enable greyscale-shader.glsl. 
+# If it is 1700 to 0900 disable greyscale-shader.glsl, otherwise enable greyscale-shader.glsl
+#
 if [ "$current_hour" -ge 2200 ] || [ "$current_hour" -le 0900 ];
-then sed -i -E '/screen_shader/s/^( *)#(.*)/\1\2/' ~/dotfiles/hypr/hyprland.org && exec emacs --batch --eval "(progn (find-file \"~/dotfiles/hypr/hyprland.org\") (save-buffer) (org-babel-tangle))" && notify-send -t 2500 'Shader' "Enabling greyscale shader for Hyprland" --icon=/home/user/Pictures/dev/Hyprland_Logo-black.png 
-    else sed -i -E '/screen_shader/s/^( *)/\1#/' ~/dotfiles/hypr/hyprland.org && exec emacs --batch --eval "(progn (find-file \"~/dotfiles/hypr/hyprland.org\") (save-buffer) (org-babel-tangle))" && notify-send -t 2500 'Shader' "Disabling greyscale shader for Hyprland" 
+then sed -i -E '/screen_shader/s/^( *)#(.*)/\1\2/' ~/dotfiles/hypr/hyprland.org && exec emacs --batch --eval "(progn (find-file \"~/dotfiles/hypr/hyprland.org\") (save-buffer) (org-babel-tangle))" 
+       sleep 5	
+       hyprctl reload
+    else sed -i -E '/screen_shader/s/^( *)/\1#/' ~/dotfiles/hypr/hyprland.org && exec emacs --batch --eval "(progn (find-file \"~/dotfiles/hypr/hyprland.org\") (save-buffer) (org-babel-tangle))" 
+	    sleep 5 
+	    hyprctl reload 
 fi
 
-# reload hyprland
 hyprctl reload
